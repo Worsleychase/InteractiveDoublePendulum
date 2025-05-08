de10e0e14418c7ade726787c8efc9b7fecf36e67
Worsleychase <worsleychase@gmail.com>
2025-05-08 20:30:48 +0000

    Force matplotlib to use interactive backend (TkAgg)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg') 
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, CheckButtons, Button
 from matplotlib.animation import FuncAnimation
